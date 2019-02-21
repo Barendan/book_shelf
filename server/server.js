0cd7336f7ef1471f929d020aa3d6fd7c67376f9e
Daniel Barenboim <barendan@gmail.com>
2019-02-21 15:29:30 -0500

    Added get and post methods;tested them using Postman

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const mongoose = require('moongoose');
+const mongoose = require('mongoose');
 const config = require('./config/config').get(process.env.NODE_ENV);
 const app = express();
 
@@ -14,9 +14,46 @@ const { Book } = require('./models/book');
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+
 // GET //
+app.get('/api/getBook',(req,res)=> {
+	let id = req.query.id;
+
+	Book.findById(id,(err,doc)=>{
+		if(err) return res.status(400).send(err);
+		res.send(doc)
+	})
+})
+
+app.get('/api/books',(req, res)=> {
+	//localhost:3001/api/books?skip=3&limit=2&order=asc
+	let skip = parseInt(req.query.skip);
+	let limit = parseInt(req.query.limit);
+	let order = req.query.order;
+
+	// ORDER = asc || desc
+	Book.find().skip(skip).sort({_id:order}).limit(limit).exec((err,doc)=>{
+		if(err) return res.status(400).send(err);
+		res.send(doc);
+	})
+
+})
+
+
+
 
 // POST //
+app.post('/api/book',(req,res)=>{
+	const book = new Book(req.body)
+
+	book.save((err,doc)=>{
+		if(err) return res.status(400).send(err);
+		res.status(200).json({
+			post:true,
+			bookId: doc._id
+		})
+	})
+})
 
 // UPDATE //
 
