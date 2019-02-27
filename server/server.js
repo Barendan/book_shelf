f33b3cc27712b7d5e0fec561f06dd18821fc8e4e
Daniel Barenboim <barendan@gmail.com>
2019-02-27 14:49:28 -0500

    Finished creating post and get routes for user creation and updating using tokens

--- server/server.js
+++ server/server.js
@@ -10,12 +10,31 @@ mongoose.connect(config.DATABASE);
 
 const { User } = require('./models/user');
 const { Book } = require('./models/book');
+const { auth } = require('./middleware/auth');
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 
 // GET //
+
+app.get('/api/auth',auth,(req,res)=>{
+	res.json({
+		isAuth:true,
+		id:req.user._id,
+		email:req.user.email,
+		name:req.user.name,
+		lastname:req.user.lastname
+	})
+})
+
+app.get('/api/logout',auth,(req,res)=>{
+	req.user.deleteToken(req.token,(err,user)=>{
+		if(err) return res.status(400).send(err);
+		res.sendStatus(200)
+	})
+})
+
 app.get('/api/getBook',(req,res)=> {
 	let id = req.query.id;
 
@@ -36,7 +55,32 @@ app.get('/api/books',(req, res)=> {
 		if(err) return res.status(400).send(err);
 		res.send(doc);
 	})
+})
 
+app.get('/api/getReviewer',(req,res)=>{
+	let id = req.query.id;
+
+	User.findById(id,(err,doc)=>{
+		if(err) return res.status(400).send(err);
+		res.json({
+			name: doc.name,
+			lastname: doc.lastname
+		})
+	})
+})
+
+app.get('/api/users',(req,res)=>{
+	User.find({},(err,users)=>{
+		if(err) return res.status(400).send(err);
+		res.status(200).send(users)
+	})
+})
+
+app.get('/api/user_posts',(req,res)=>{
+	Book.find({ownerId:req.query.user}).exec((err,docs)=>{
+		if(err) return res.status(400).send(err);
+		res.send(docs)
+	})
 })
 
 
@@ -51,6 +95,43 @@ app.post('/api/book',(req,res)=>{
 			bookId: doc._id
 		})
 	})
+})
+
+app.post('/api/register',(req,res)=>{
+	const user = new User(req.body);
+
+	user.save((err,doc)=>{
+		if(err) return res.json({success:false});
+		res.status(200).json({
+			success:true,
+			user:doc
+		})
+	})
+})
+
+app.post('/api/login',(req,res)=>{
+	User.findOne({'email':req.body.email},(err,user)=>{
+		if(!user) return res.json({isAuth:false,message:'Auth failed, email not found'})
+
+		user.comparePassword(req.body.password,(err,isMatch)=>{
+			if(!isMatch) return res.json({
+				isAuth: false,
+				message: 'Wrong password'
+			});
+
+			user.generateToken((err,user)=>{
+				if(err) return res.status(400).send(err);
+				res.cookie('auth',user.token).json({
+					isAuth:true,
+					id: user._id,
+					email: user.email
+				})
+			})
+
+		})
+	})
+
+
 })
 
 // UPDATE //
