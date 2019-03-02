c1781ed821e0b8484abdb8ed746283be4753e76a
Daniel Barenboim <barendan@gmail.com>
2019-03-02 18:57:07 -0500

    created login form and functionality

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -56,3 +56,22 @@ export function clearBookWithReviewer(){
 		}
 	}
 }
+
+
+
+// USER ACTIONS
+
+
+export function loginUser({email,password}){
+	const request = axios.post('/api/login',{email,password})
+				.then(response => response.data)
+
+	return {
+		type:'USER_LOGIN',
+		payload: request
+	}
+}
+
+
+
+
